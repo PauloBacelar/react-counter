75c9043a6bb813eaf4a6386f8c693a52a47389bf
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-03-30 11:26:10 -0300

    Made basic counter and buttons structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <h1>Hello, World!</h1>;
+// Styles
+const counterStyles = {
+  textAlign: "center",
+  marginBottom: 25
+};
+
+const containerStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  height: "100vh",
+  width: "100vw",
+};
+
+// Main functions
+class App extends React.Component {
+  render() {
+    return (
+      <div style={containerStyles}>
+        <div className="counter">
+          <h1 style={counterStyles}>0</h1>
+        </div>
+
+        <div className="buttons">
+          <button>Decrease</button>
+          <button>Increase</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
